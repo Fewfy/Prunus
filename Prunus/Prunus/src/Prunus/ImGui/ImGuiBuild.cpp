5c8363f045fffeeeb49226ba96a377cb29df8cf0
Yao Fu <fuyaozju@163.com>
2023-04-13 00:50:56 +0800

    reference the correct file

--- Prunus/Prunus/src/Prunus/ImGui/ImGuiBuild.cpp
+++ Prunus/Prunus/src/Prunus/ImGui/ImGuiBuild.cpp
@@ -2,5 +2,5 @@
 
 #define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
 #include <glad/glad.h>
-#include "imgui_impl_opengl3.cpp"
-#include "imgui_impl_glfw.cpp"
+#include "backends/imgui_impl_opengl3.cpp"
+#include "backends/imgui_impl_glfw.cpp"
--- Prunus/Prunus/src/Prunus/ImGui/ImGuiLayer.cpp
+++ Prunus/Prunus/src/Prunus/ImGui/ImGuiLayer.cpp
@@ -4,8 +4,8 @@
 #include "imgui.h"
 
 #define IMGUI_IMPL_API
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 
 #include "Prunus/Application.h"
 
